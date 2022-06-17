c1d07cc3d8b999e4dad8b269593c22eb991988ec
MAX <debaev@list.ru>
2022-06-17 15:51:31 +0500

    fix: add msg on error on reading a file

--- src/commands/readFilesContent.js
+++ src/commands/readFilesContent.js
@@ -4,9 +4,15 @@ import { join, isAbsolute } from "node:path";
 export const readFileContent = async (currentDir, targetFilePath) => {
   if (isAbsolute(targetFilePath)) {
     let readStream = createReadStream(targetFilePath, "utf8");
-  readStream.on("data", (result) => process.stdout.write(`${result}\n`));
+    readStream.on("data", (result) => process.stdout.write(`${result}\n`));
+    readStream.on('error', () => {
+      console.log('Operation failed: The path to the file is broken!');
+    })
   } else {
     let readStream = createReadStream(join(currentDir, targetFilePath), "utf8");
     readStream.on("data", (result) => process.stdout.write(`${result}\n`));
+    readStream.on('error', () => {
+      console.log('Operation failed: The path to the file is broken!');
+    })
   }
 };
