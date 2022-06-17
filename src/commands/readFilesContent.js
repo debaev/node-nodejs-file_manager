6883eec204a067714cbf8059218bb39e00d14cfa
MAX <debaev@list.ru>
2022-06-17 15:08:22 +0500

    fix: add reading functionality for file of absolute path

--- src/commands/readFilesContent.js
+++ src/commands/readFilesContent.js
@@ -1,7 +1,12 @@
 import { createReadStream } from "node:fs";
-import { join } from "node:path";
+import { join, isAbsolute } from "node:path";
 
 export const readFileContent = async (currentDir, targetFilePath) => {
-  let readStream = createReadStream(join(currentDir, targetFilePath), "utf8");
+  if (isAbsolute(targetFilePath)) {
+    let readStream = createReadStream(targetFilePath, "utf8");
   readStream.on("data", (result) => process.stdout.write(`${result}\n`));
+  } else {
+    let readStream = createReadStream(join(currentDir, targetFilePath), "utf8");
+    readStream.on("data", (result) => process.stdout.write(`${result}\n`));
+  }
 };
